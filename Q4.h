cfb9b67cd71e8b62e5bae998808021778451cc19
Nathaniel <nathanieljwarner@gmail.com>
2022-02-19 10:31:21 -0800

    Add question 4

--- Q4.h
+++ Q4.h
@@ -1,3 +1,11 @@
 int Q4() {
-	return 4;
+    printf("What is your favorite place to be?\n");
+    printf("(1) Class\n");
+    printf("(2) Parking 'Struct'ure\n");
+    printf("(3) 'int'ernet cafe\n");
+    printf("(4) 'float'ing along a lazy river\n");
+    printf("(5) shopping for shoe 'strings'\n");
+    int answer = 0;
+    scanf("%d", &answer);
+	return answer;
 }
