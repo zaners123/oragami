f5764313b8416ddf7c4a41a17304b41833adc0c2
zaners123 <zaners12345@gmail.com>
2022-02-19 09:54:23 -0800

    Initial information! YAY!

---
+++ main.c
@@ -0,0 +1,33 @@
+// TEAMNAME Team Oragami
+#include <stdio.h>
+int main() {
+	printf("Hello! Welcome to Team Oragami!\n");
+	printf("We are going to spread awareness\n");
+	printf("Answer these questions to find out the best way you could spread WSU awareness!\n");
+	printf("Question 1: Which word most describes you?\n");
+	printf("1. Volatile\n");
+	printf("2. Private\n");
+	printf("3. Protected\n");
+	printf("4. Public\n");
+	int Q1;
+	scanf("%d",&Q1);
+
+
+	int Q2;
+	scanf("%d",&Q2);
+
+
+	int Q3;
+	scanf("%d",&Q3);
+
+
+
+
+
+	if (Q1==1 && Q2==2 && Q3==3) {
+		printf("You could spread awareness through compassion!");
+	}
+
+
+	return 0;
+}
