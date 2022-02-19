d7a86fa789418fe153752c7657eb42eeb9b89dd7
Nathaniel <nathanieljwarner@gmail.com>
2022-02-19 10:46:42 -0800

    Provide an answer based on question input

--- answer.h
+++ answer.h
@@ -3,11 +3,19 @@
 void answer(int Q1, int Q2, int Q3, int Q4) {
 
 	int score = Q1+Q2+Q3+Q4;
-	if (score > 5) {
+    if (score < 5) {
+        printf("You're doing great! You could spread awareness through compassion!");
+    }
+	else if (score < 8) {
 		printf("Learn about the WSU community!");
 	}
-
-	if (Q1==1 && Q2==2 && Q3==3) {
-		printf("You could spread awareness through compassion!");
-	}
+    else if (score < 12) {
+        printf("Maybe try to get involved with some student sponsored clubs!");
+    }
+    else if (score < 16) {
+        printf("Now would be a great time to seek out WSU mental health services!");
+    }
+    else{
+        printf("Seek help immediately! Call 911!");
+    }
 }
