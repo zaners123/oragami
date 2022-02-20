4aecf0684dd5d839b68632e205f4d8f394f23259
Chris <c.vidalgarcia@wsu.edu>
2022-02-19 18:06:24 -0800

    Temp add Q2

--- Q2.h
+++ Q2.h
@@ -1,3 +1,14 @@
+#ifndef Q2
+#define Q2
 int Q2() {
-	return 3;
+    printf("Question 2: What's your favorite meat?\n");
+    printf("1. Chicken\n");
+    printf("2. Beef\n");
+    printf("3. Pork\n");
+    printf("4. Other\n");
+
+    int input;
+    scanf_s("%d", &input);
+	return 2;
 }
+#endif
